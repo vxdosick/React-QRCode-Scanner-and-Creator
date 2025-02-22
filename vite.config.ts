27212d10be5153ba53d1891005f964073f6e2fa7
vxdosick <162177884+vxdosick@users.noreply.github.com>
2025-02-22 22:02:43 +0100

    fix: Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,5 +5,5 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  base: '/react-ts-tailwind-qrcode',
+  base: '/',
 })
